e00440da042d359f33ebead291a72f946c471475
Maxim Nikitin <nmax_2002@mail.ru>
2022-11-08 21:22:45 +0300

    add-logback: tuned logback config.

---
+++ src/main/java/lesson3/Test.java
@@ -0,0 +1,13 @@
+package lesson3;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Test {
+
+    private static final Logger log = LoggerFactory.getLogger(Test.class);
+
+    public static void main(String[] args) {
+        log.info("Hello {}", "world!");
+    }
+}
